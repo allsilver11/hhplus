c977e324e31894d1523d2c929041c5244bd44e03
allsilver <allsilver@infobank.net>
2023-12-20 19:02:19 +0900

    주석 추가

--- src/test/java/com/service/concert/controller/WalletControllerTest.java
+++ src/test/java/com/service/concert/controller/WalletControllerTest.java
@@ -8,7 +8,7 @@ class WalletControllerTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 }
--- src/test/java/com/service/concert/service/TokenServiceTest.java
+++ src/test/java/com/service/concert/service/TokenServiceTest.java
@@ -8,7 +8,7 @@ class TokenServiceTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 }
